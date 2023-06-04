fa2a0e04651ba2c2121693fbfc26b5b7e5ba9e43
jihane1107 <jihaneafoukal93@gmail.com>
2023-06-04 12:40:08 -0700

    task1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * list_len function: returns the number of elements in a linked list.
+ * @h: pointer to one element on a linked list
+ * Return: number of elements defined here by longueur
+ * this code is made by: jihane1107.
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t longueur = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		longueur++;
+	}
+	return (longueur);
+}
